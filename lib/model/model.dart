98ab71eabd66412beedfa332b2877ce15b0216db
Disha gupta <109867094+Disha152@users.noreply.github.com>
2023-03-10 02:44:45 +0530

    updated ui  + mic feature + BLoc state management

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -1,11 +1,11 @@
 enum ChatMessageType { user, bot }
 
 class ChatMessage {
+  final String text;
+  final ChatMessageType chatMessageType;
+
   ChatMessage({
     required this.text,
     required this.chatMessageType,
   });
-
-  final String text;
-  final ChatMessageType chatMessageType;
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import cloud_firestore
+import firebase_auth
+import firebase_core
+import path_provider_foundation
+import shared_preferences_foundation
+import speech_to_text_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
 }
